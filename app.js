3aa011c6c5258d9562cc0db9c17c2ca7dc2147cf
poonam ghule <poonamghule33@gmail.com>
2025-04-14 00:10:05 +0530

    Review.html

--- app.js
+++ app.js
@@ -1,15 +1,76 @@
-const colors=["red","green","rgb(1,45,20)","#f1c5D"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const reviews =[
+    {
+        id:1,
+        name:"Nemat",
+        job: "Developer",
+        img: "img/pallaviphoto.jpg",
+        text: "Hello I am Here to give you a review to ypur cources very good content",
+     
+    },{
+        id:1,
+        name:"joby",
+        job: " web Developer",
+        img: "img/nature.jpg",
+        text: "Hello I am Here to give you a review to ypur cources very good content",
+    
 
-btn.addEventListener("click",function(){
-  
-    const randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = color[randomNumber];
-    color.textContent = color[randomNumber];
+    },{
+        id:1,
+        name:"poonam",
+        job: " android Developer",
+        img: "img/th.jpg",
+        text: "Hello I am Here to give you a review to ypur cources very good content",
 
-});
+    },{
+        id:1,
+        name:"siya",
+        job: "linux Developer",
+        img: "img/istockphoto-2166282428-612x612.webp",
+        text: "Hello I am Here to give you a review to ypur cources very good content",
 
-function getRandomNumber(){
-    return Math.floor(Math.random()*colors.length);
-}
+    }
+    ];
+
+
+    const img= document.getElementById("person-img");
+    const author= document.getElementById("author");
+    const job= document.getElementById("job");
+    const info= document.getElementById("info");
+
+
+    const perBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+
+    let.currentItem = 0;
+    
+    window.addEventListener("DOMContentLoaded",function(){
+    ShowPerson(currentItem);
+    });
+
+
+ function ShowPerson(person){
+        const item = reviews[currentItems];
+   
+    img.src = item.img;
+    author.textContent=item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+
+    }
+        nextBtn.addEventListener("click",function(){
+            currentItem++;
+            if(currentItem > reviews.length -1){
+                currentItem = 0;
+            }
+
+            ShowPerson(currentItem);
+
+        });
+        perBtn.addEventListener("click", function(){
+            currentItem--;
+            if(currentItem < 0){
+                currentItem = reviews.length -1;
+                
+            }
+            ShowPerson(currentItem);
+        })
